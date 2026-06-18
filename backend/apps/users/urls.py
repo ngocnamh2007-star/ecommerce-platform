5c8a14de42adcc65745c314e4b7c4937e138b2d1
Phạm Ngọc Nam <ngocnamh2007@gmail.com>
2026-06-18 21:59:57 +0700

    Add user URLs

---
+++ backend/apps/users/urls.py
@@ -0,0 +1,17 @@
+"""
+User app URLs
+"""
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import UserViewSet, CustomTokenObtainPairView
+
+router = DefaultRouter()
+router.register(r'', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
